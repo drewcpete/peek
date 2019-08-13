069175f7a757b1478968ae4ad89e6b46cbd975fd
Andrew Peterson and dominic montelongo <andrew@>
2019-08-13 09:15:46 -0700

    Add img and fadetoggle function to js

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,23 @@ $(document).ready(function(){
   $("p").click(function(){
     $("img").show();
   });
-});
+
 // $(document).ready(function(){
 //   $("p").click(function(){
 //     $(".bottle-see").show();
 //     $(".bottle-hide").hide();
 //   });
 // })
-$(document).ready(function() {
-  $("p").click(function() {
-    $(".bottle-see").toggle();
+
+  $(".clickable").click(function() {
+    $("#bottle-see").toggle();
     $(".bottle-hide").toggle();
+  });
+
+
+
+  $(".wallet-click").click(function() {
+    $("#wallet-here").fadeToggle();
+    $("#wallet-gone").fadeToggle();
   });
 });
